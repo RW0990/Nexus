a3377ee77bdf8966c5e7964ea2818dd4572e9bb8
RW0990 <whiteryan2599@icloud.com>
2026-07-12 21:24:19 +0100

    Add files via upload

---
+++ Nexus/src/main/java/week9/nexus/Nexus.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package week9.nexus;
+
+/**
+ *
+ * @author ryanwhite
+ */
+public class Nexus {
+
+    public static void main(String[] args) {
+        LogIn login1= new LogIn("", "");
+        login1.newLogin();
+        System.out.println(login1);
+    }
+}
