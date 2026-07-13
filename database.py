a82f9b3f2f7ff9cf12e6ba668cd889598f32bcb3
RW0990 <whiteryan2599@icloud.com>
2026-07-13 20:25:33 +0100

    Add files via upload

--- database.py
+++ database.py
@@ -1,17 +1,25 @@
 #importing libraries
+import certifi
 from pymongo import MongoClient
 
-MongoConnect=MongoClient()
-from mongodblink import mongodb
 
-MongoConnect=MongoClient(mongodb)['Link']
+uri="mongodb+srv://x23353252_db_user:<db_password>@nexusdatabase.kg5edmm.mongodb.net/"
+#e34we9ZpboRlMUJB
+client=MongoClient(uri,tlsCAFile=certifi.where())
 
-cluster=MongoConnect['NexusDatabase']
-user=cluster['']
+try:
+    client.admin.command('ping')
+    print("Connected to database.")
+except Exception as e:
+    print(e)
+
+database=client['NexusDatabase']
+cluster=database['NexusDatabase']
 
 data={
     "Username":"user1",
     "Password":"password1",
     "email":"email1@email.com"
 }
-user.insert_one(data)
+cluster.insert_one(data)
+print(data)
